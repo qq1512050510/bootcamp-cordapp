e4bb0524ca99735238f4aeacdcdd96fd02d0075e
jiangyupeng <765819328@qq.com>
2020-05-15 15:14:00 +0800

    Contract Junit

--- gradlew.bat
+++ gradlew.bat
@@ -12,6 +12,8 @@ set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
+%设置字符集%
+set DEFAULT_JVM_OPTS="-Dfile.encoding=UTF-8"
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
--- src/main/java/java_bootcamp/IOU/IOUContract.java
+++ src/main/java/java_bootcamp/IOU/IOUContract.java
@@ -1,11 +1,61 @@
 package java_bootcamp.IOU;
 
+import net.corda.core.contracts.Command;
 import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.Contract;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.Party;
+import net.corda.core.transactions.LedgerTransaction;
+import org.jetbrains.annotations.NotNull;
+
+import java.security.PublicKey;
+import java.util.List;
 
 /* Our contract, governing how our state will evolve over time.
  * See src/main/java/java_examples/ArtContract.java for an example. */
-public class IOUContract {
+public class IOUContract implements Contract {
     public static String ID = "java_bootcamp.IOU.IOUContract";
+
+
+    @Override
+    public void verify(@NotNull LedgerTransaction tx) throws IllegalArgumentException {
+        if (tx.getCommands().size() != 1) {
+            throw new IllegalArgumentException("Transaction must have one command");
+        }
+
+
+        Command command = tx.getCommand(0);
+        CommandData commandType = command.getValue();
+        List<PublicKey> requiredKey = command.getSigners();
+        if (commandType instanceof Commands.Issue) {
+            if (tx.getInputStates().size() != 0) {
+                throw new IllegalArgumentException("Register transaction must have no inputs");
+            }
+            if (tx.getOutputStates().size() != 1) {
+                throw new IllegalArgumentException("Register transaction must have one output");
+            }
+
+            //1. output is Iou$State?
+            if (!(tx.getOutput(0) instanceof IOUState)) {
+                throw new IllegalArgumentException("Output state must be a IOUState");
+            }
+            IOUState outputState = (IOUState) tx.getOutput(0);
+            //2. amount is positive
+            if (outputState.getAmount() <= 0) {
+                throw new IllegalArgumentException("Issue Amount must be positive number");
+            }
+            //3.命令内容为Issue
+
+            //4. Issue signature
+            Party issue = outputState.getIssuer();
+            PublicKey publicKey = issue.getOwningKey();
+            if (!requiredKey.contains(publicKey)) {
+                throw new IllegalArgumentException("The signature must contains the issue");
+            }
+        } else {
+            throw new IllegalArgumentException("Unrecognized command");
+        }
+    }
 
     public interface Commands extends CommandData {
         class Issue implements Commands {
